3a947b13396b0cb340b73a9a396a96a20f2903e1
dseum <dseum22@gmail.com>
2024-01-06 02:18:47 +0400

    Updated `nvim` plugin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -130,7 +130,6 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.2",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
       { "<leader>pf", "<cmd>Telescope find_files<cr>" },
