7bbb0a2be3a43bdf32b8b99f6adf1e99f77ebaa0
dseum <dseum22@gmail.com>
2024-02-11 22:24:20 -0500

    feat(nvim): removes autopairs plugin

--- nvim/lua/plugins/text.lua
+++ nvim/lua/plugins/text.lua
@@ -2,37 +2,6 @@ return {
   "tpope/vim-sleuth",
   "tpope/vim-surround",
   {
-    "echasnovski/mini.pairs",
-    opts = {
-      mappings = {
-        ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][^%a%d]" },
-        ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][^%a%d]" },
-        ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][^%a%d]" },
-        [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
-        ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
-        ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
-        ['"'] = {
-          action = "closeopen",
-          pair = '""',
-          neigh_pattern = '[^%a%d\\"][^%a%d]',
-          register = { cr = false },
-        },
-        ["'"] = {
-          action = "closeopen",
-          pair = "''",
-          neigh_pattern = "[^%a%d\\<'][^%a%d]",
-          register = { cr = false },
-        },
-        ["`"] = {
-          action = "closeopen",
-          pair = "``",
-          neigh_pattern = "[^%a%d\\`][^%a%d]",
-          register = { cr = false },
-        },
-      },
-    },
-  },
-  {
     "JoosepAlviste/nvim-ts-context-commentstring",
     init = function()
       vim.g.skip_ts_context_commentstring_module = true
