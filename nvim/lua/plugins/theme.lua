2b7da76a8b625bd0def2710268f4750d1ea4702a
dseum <dseum22@gmail.com>
2024-05-16 17:50:47 -0400

    fix(nvim): nvim 0.10.0

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -6,7 +6,12 @@ return {
       require("tokyonight").setup({
         style = "night",
         on_highlights = function(hl, c)
-          hl.LineNr.fg = "#7D85A7"
+          hl.LineNrAbove = {
+            fg = c.dark5,
+          }
+          hl.LineNrBelow = {
+            fg = c.dark5,
+          }
           hl.TelescopePromptBorder =
             { fg = c.border_highlight, bg = c.bg_float }
           hl.TelescopePromptTitle = { fg = c.border_highlight, bg = c.bg_float }
