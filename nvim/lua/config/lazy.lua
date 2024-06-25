1af8025814d19ec3f430659fcdc2bb542c79a0f8
dseum <dseum22@gmail.com>
2024-06-25 11:38:17 +0900

    feat(nvim): lazy 11

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,17 +1,22 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
+    lazyrepo,
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {
+require("lazy").setup({
+    spec = {
+    -- import your plugins
+    { import = "plugins" },
+},
   change_detection = {
     notify = false,
   },
@@ -23,16 +28,17 @@ require("lazy").setup("plugins", {
       cmd = "",
       config = "",
       event = "",
+      favorite = "",
       ft = "",
       init = "",
       import = "",
       keys = "",
+      lazy = "",
       plugin = "",
       runtime = "",
+      require = "",
       source = "",
       start = "",
-      task = "",
-      lazy = "",
     },
   },
 })
