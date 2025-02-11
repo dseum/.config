df0554d5f160db5b9c25735203346b879540c7c4
dseum <dseum.me@gmail.com>
2025-02-11 01:21:08 -0500

    feat(nvim): add haskell-language-server

--- nvim/lua/plugins/process.lua
+++ nvim/lua/plugins/process.lua
@@ -115,6 +115,7 @@ return {
         docker_compose_language_service = { "docker-compose-language-server" },
         dockerls = { "dockerfile-language-server" },
         gopls = { "gopls" },
+        hls = { "haskell-language-server" },
         html = { "html-lsp" },
         jsonls = { "json-lsp" },
         lua_ls = {
