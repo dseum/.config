c834c8b1c58b77b43d97f756bd6abbf44e4fa870
dseum <dseum.me@gmail.com>
2025-04-11 20:47:21 -0400

    chore(nvim): updates

--- nvim/lua/plugins/process.lua
+++ nvim/lua/plugins/process.lua
@@ -46,7 +46,7 @@ return {
       "folke/lazydev.nvim",
       "rafamadriz/friendly-snippets",
     },
-    version = "1.0.0",
+    version = "1.1.1",
     opts = {
       sources = {
         default = { "lazydev", "lsp", "path", "snippets", "buffer" },
